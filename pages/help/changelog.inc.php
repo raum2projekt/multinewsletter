87057e99b898ac7889e1e29c4b1041cb83c56832
tobias <tobias@UbuntuXPS>
2016-01-23 07:38:52 +0100

    Changelog aktualisiert.

--- pages/help/changelog.inc.php
+++ pages/help/changelog.inc.php
@@ -1,12 +1,16 @@
 <h1>MultiNewsletter Changelog</h1>
 
+<p>2.2.2 DEV:</p>
+<ul>
+	<li>HTMLBody im Archiv nun auch vom Typ Longtext sein.</li>
+</ul>
 <p>2.2.1:</p>
 <ul>
 	<li>Funktion MultiNewsletterUser:initByMail() hat gibt jetzt bei nicht
 	gefundener E-Mailadresse korrekt false zurück.</li>
 	<li>Anmeldung und Abmeldung verbessert.</li>
 	<li>Platzhalter für Links beginnen und enden jetzt mit +++.</li>
-<</ul>
+</ul>
 <p>2.2:</p>
 <ul>
 	<li>Bessere Dokumentation für Aktivierungslink. Thx @missmissr</li>
