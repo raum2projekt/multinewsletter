54c6feba190d27c9872451e1f03801c125ae887f
TobiasKrais <tk@design-to-use.de>
2016-09-05 09:58:04 +0200

    Ein Slash zu viel

--- langpresets.inc.php
+++ langpresets.inc.php
@@ -29,7 +29,7 @@ $REX['MULTINEWSLETTER_LANG_PRESETS'] = array(
 		'confirmsubject' => 'Bitte bestätigen Sie Ihre Newsletter Anmeldung',
 		'confirmcontent' => '<p>Hallo +++TITLE+++ +++GRAD+++ +++FIRSTNAME+++ +++LASTNAME+++,</p>
 <br>
-<p>bitte bestätigen Sie aus Sicherheitsgründen Ihre Anmeldung zum Newsletter. Klicken Sie dazu einfach auf den folgenden Link:  <a href=\\"+++AKTIVIERUNGSLINK+++\\">+++AKTIVIERUNGSLINK+++</a></p>
+<p>bitte bestätigen Sie aus Sicherheitsgründen Ihre Anmeldung zum Newsletter. Klicken Sie dazu einfach auf den folgenden Link:  <a href=\"+++AKTIVIERUNGSLINK+++\">+++AKTIVIERUNGSLINK+++</a></p>
 <br>
 <p>TIPP: Damit unsere E-Mails nicht ungewollt in den Spam-Ordner verschoben oder gelöscht werden, nehmen Sie uns einfach in Ihr persönliches Adressbuch auf.</p>
 <p>Dazu klicken Sie je nach verwendetem E-Mail-Programm entweder</p>
@@ -81,7 +81,7 @@ $REX['MULTINEWSLETTER_LANG_PRESETS'] = array(
 		'confirmsubject' => 'Please confirm your registration',
 		'confirmcontent' => '<p>Dear +++TITLE+++ +++GRAD+++ +++FIRSTNAME+++ +++LASTNAME+++,</p>
 <br>
-<p>for legal reasons – please confirm your newsletter registration by clicking on the following link: <a href=\\"+++AKTIVIERUNGSLINK+++\\">+++AKTIVIERUNGSLINK+++</a></p>
+<p>for legal reasons – please confirm your newsletter registration by clicking on the following link: <a href=\"+++AKTIVIERUNGSLINK+++\">+++AKTIVIERUNGSLINK+++</a></p>
 <p>HINT: Just to ensure that our E-mails are not inadvertently pushed to the spam folder or deleted, please simply add us in your personal address book.</p>
 <p>For this, click on either of the following, depending on the used E-Mail program:</p>
 <ul>
