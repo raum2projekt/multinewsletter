b252df30215d5f31590f3c7bf0e7dda2c67dfcfd
Alex Platter <a.platter@kreatif.it>
2017-09-04 15:26:10 +0200

    mailchimp api added

--- boot.php
+++ boot.php
@@ -1,5 +1,15 @@
 <?php
 if (rex::isBackend() && is_object(rex::getUser())) {
-	rex_view::addJsFile($this->getAssetsUrl('multinewsletter.js'));
-	rex_perm::register('multinewsletter[]', rex_i18n::msg('multinewsletter_addon_short_title'));
+    rex_view::addJsFile($this->getAssetsUrl('multinewsletter.js'));
+    rex_perm::register('multinewsletter[]', rex_i18n::msg('multinewsletter_addon_short_title'));
+
+    rex_extension::register('REX_FORM_SAVED', function ($ep) {
+
+        if (MultinewsletterMailchimp::isActive()) {
+            $user_id   = rex_get('entry_id', 'int');
+            $User      = new MultinewsletterUser($user_id);
+            $User->save();
+        }
+        return $ep->getSubject();
+    });
 }
