93a574d88fbd5aaa1201f16dfcb4e3fd77fcd8f3
Alex Platter <a.platter@kreatif.it>
2017-09-28 11:21:04 +0200

    assets added

--- boot.php
+++ boot.php
@@ -3,6 +3,7 @@
 if (rex::isBackend() && is_object(rex::getUser())) {
 
     rex_view::addJsFile($this->getAssetsUrl('multinewsletter.js'));
+    rex_view::addCssFile($this->getAssetsUrl('general.css'));
     rex_perm::register('multinewsletter[]', rex_i18n::msg('multinewsletter_addon_short_title'));
 
     if ($this->getConfig('use_yform')) {
