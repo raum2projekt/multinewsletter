ce4e4d37d225dac6350eaef349c95a98975e8db0
Tobias Krais <tk@design-to-use.de>
2018-04-25 22:17:15 +0200

    DSGVO Anpassungen

--- pages/index.php
+++ pages/index.php
@@ -15,7 +15,7 @@ function raw_field($label, $content) {
 
 echo rex_view::title($this->i18n('multinewsletter_addon_short_title'));
 
-if (rex_config::get('d2u_helper', 'article_id_privacy_policy', 0) == 0) {
+if (rex_config::get('d2u_helper', 'article_id_privacy_policy', 0) == 0 || rex_config::get('d2u_helper', 'article_id_impress', 0) == 0) {
 	print rex_view::warning(rex_i18n::msg('d2u_helper_gdpr_warning'));
 }
 
