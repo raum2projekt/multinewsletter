41cbae88921b7db44fc162277cc54fdda83c5be8
Tobias Krais <tk@design-to-use.de>
2018-09-13 16:21:50 +0200

    Automatic send available

--- pages/help.faq.php
+++ pages/help.faq.php
@@ -10,34 +10,36 @@
 		https://www.heise.de/-4023584</a>.</p>
 	<p>Bei der Benutzung von MailChimp muss der Benutzer informiert werden, dass
 		seine Daten anden Betreiber von MailChimp weiter gegeben werden.</p>
-
+	<br>
 	<p><strong>Frage: Meine Aktivierungsmail wird nicht verschickt. Warum?</strong></p>
 	<p>Das könnte mehrere Ursachen haben. Ist der <a href="<?php print rex_url::backendPage('phpmailer/config'); ?>">
 		PHPMailer</a> korrekt konfiguriert?
 		Sind in den <a href="<?php print rex_url::backendPage('multinewsletter/config'); ?>">
 		MultiNewsletter Einstellungen die Übersetzungen</a> eingepflegt?</p>
-
+	<br>
 	<p><strong>Frage: Warum ist der Link in der Bestätigungsmail in manchen Mailprogrammen
 		nicht als Link aktiviert?</strong></p>
 	<p>Der Link wird nur dann immer aktiviert, wenn er in den <a href="<?php print rex_url::backendPage('multinewsletter/config'); ?>">
 		Einstellungen, bei den Übersetzungen</a> unter "Text der
 		Bestätigungsmail" auch als HTML-Link programmiert wurde. Bitte deshalb
 		das "a href=..." nicht vergessen!</p>
-
+	<br>
 	<p><strong>Frage: Der Link in der Aktivierungsmail ist zwar aktiviert, funktioniert
 		aber nicht. Warum?</strong></p>
 	<p>In den <a href="<?php print rex_url::backendPage('system/settings'); ?>">
 		Redaxo Systemeinstellungen</a> muss im Feld "URL der Webseite" die URL inklusive
 		http:// (oder https://) und am Ende / eingegeben werden.</p>
-
+	<br>
 	<p><strong>Frage: Warum wird die Aktivierungsmail nicht verschickt, die anderen
 			Mails aber schon?</strong></p>
 	<p>Wenn die <a href="<?php print rex_url::backendPage('multinewsletter/config'); ?>">
 			MultiNewsletter Spracheinstellungen</a> eingegeben sind sollte dieser
 			"Fehler" behoben sein.</p>
-
+	<br>
 	<p><strong>Gibt es eine Möglichkeit aus dem PHP Code heraus den Versand anzustoßen?</strong></p>
-	<p>Ja. Durch folgende Methode:</p>
+	<p>Ja. Hierzu muss das CronJob Addon aktiviert sein und in den Einstellungen
+		eine Admin E-Mail Adresse hinterlegt sein, sowie die Autosend Option
+		aktiviert werden. Durch folgende Methode kann der Versand gestartet werden:</p>
 	<pre>MultinewsletterNewsletterManager::autosend($group_ids, $article_id, $fallback_clang_id, $recipient_ids = [], $attachments = '')</pre>
 	<p>Hinweise zu den Parametern:</p>
 	<ul>
